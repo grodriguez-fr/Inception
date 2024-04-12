2978c8e2ca8330d43c33ffb2939eb79a7842fb11
Guillaume Rodriguez <gurodrig@z3r6p5.42lyon.fr>
2024-04-12 16:33:52 +0200

    tqt

--- srcs/requirements/wordpress/tools/start_wordpress.sh
+++ srcs/requirements/wordpress/tools/start_wordpress.sh
@@ -1,23 +1,23 @@
 wp core download --allow-root
 
-wp config create --dbname=${SQL_DATABASE} \
-   --dbuser=${SQL_USER} \
-   --dbpass=${SQL_USER_PASSWORD} \
-   --dbhost=mariadb:3306 --allow-root \
-   --path='/var/www/wordpress'
+wp config create --allow-root \
+   --dbname=$SQL_DATABASE \
+   --dbuser=$SQL_USER \
+   --dbpass=$SQL_USER_PASSWORD \
+   --dbhost=mariadb:3306 \
 
 wp core install --allow-root \
-   --url=gurodrig.42.fr \
-   --title=${WP_TITLE} \
+   --url="gurodrig.42.fr" \
+   --title=$WP_TITLE \
    --admin_user=$WP_ADMIN_LOGIN \
    --admin_password=$WP_ADMIN_PASSWORD \
    --admin_email=$WP_ADMIN_EMAIL
 	
 
 wp user create --allow-root \
-   ${WP_USER_LOGIN} \
-   ${WP_USER_EMAIL} \
-   --user_pass=${WP_USER_PASSWORD};
+   $WP_USER_LOGIN \
+   $WP_USER_EMAIL \
+   --user_pass=$WP_USER_PASSWORD;
 
 mkdir -p /run/php
 
